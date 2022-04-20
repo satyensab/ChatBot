79535aca9c5bcba3b4bde809bd95fe0709865ac4
Satyen Sabnis <97633870+satyensab@users.noreply.github.com>
2022-04-20 21:00:05 +0000

    added comments

--- Chat Bot/train_chatbot.py
+++ Chat Bot/train_chatbot.py
@@ -16,7 +16,7 @@ from keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
 import random
 
-#defining variables
+#variables
 words=[]
 classes = []
 documents = []
@@ -24,67 +24,70 @@ ignore_words = ['?', '!']
 data_file = open('intents.json').read()
 intents = json.loads(data_file)
 
-#data cleaning
+#cleans data 
 for intent in intents['intents']:
     for pattern in intent['patterns']:
 
-        #tokenize each word
+        # splits a large sample of text into words
         w = nltk.word_tokenize(pattern)
         words.extend(w)
-        #add documents in the corpus
+        #add documents to the multiple collection of text data
         documents.append((w, intent['tag']))
 
-        # add to our classes list
+        # adds tags to the list
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-# lemmaztize and lower each word and remove duplicates 
+# groupes together the different inflected forms of a word so they can be analyzed as a single item and then lowers each word and removes any duplicates 
 words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
 words = sorted(list(set(words)))
-# sort classes
+
+# sorts the classes alphabetically
 classes = sorted(list(set(classes)))
-# documents = combination between patterns and intents
+# the documents are the combination between patterns and intents
 print (len(documents), "documents")
-# classes = intents
+# the classes represent the intents, computer language
 print (len(classes), "classes", classes)
-# words = all words, vocabulary
+# the vocabulary of the computer language
 print (len(words), "unique lemmatized words", words)
 
-
+#converts a Python object into a json string.
 pickle.dump(words,open('words.pkl','wb'))
 pickle.dump(classes,open('classes.pkl','wb'))
 
-# create our training data
+#now that we have got the neccessary computer responses we now create our training data
 training = []
-# create an empty array for our output
+
+#output array
 output_empty = [0] * len(classes)
-# training set, bag of words for each sentence
+
+# the training set - the data we train to predict computer response
 for doc in documents:
-    # initialize our bag of words
+    # initialize the collection of words
     bag = []
-    # list of tokenized words for the pattern
     pattern_words = doc[0]
-    # lemmatize each word - create base word, in attempt to represent related words
+    # a root word to correlate with related words
     pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
-    # create our bag of words array with 1, if word match found in current pattern
+    # adds related words into a bag of words
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
     
-    # output is a '0' for each tag and '1' for current tag (for each pattern)
     output_row = list(output_empty)
     output_row[classes.index(doc[1])] = 1
     
     training.append([bag, output_row])
-# shuffle our features and turn into np.array
+  
+#turns training data into a numpy array
 random.shuffle(training)
 training = np.array(training)
-# create train and test lists. X - patterns, Y - intents
+
+# the train and test lists
 train_x = list(training[:,0])
 train_y = list(training[:,1])
 print("Training data created")
 
 
-# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
+# Create models
 # equal to number of intents to predict output intent with softmax
 model = Sequential()
 model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
@@ -93,7 +96,7 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+# Compile models
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
